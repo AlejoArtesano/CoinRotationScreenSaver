fa2559335a37b0bb79536358ac55db9c7ffd77ba
Alejo Artesano <45542827+AlejoArtesano@users.noreply.github.com>
2024-04-10 20:53:03 +0400

    Initial Commit

---
+++ CoinRotationScreenSaver/CoinRotationScreenSaverView.h
@@ -0,0 +1,12 @@
+//
+//  CoinRotationScreenSaverView.h
+//  CoinRotationScreenSaver
+//
+//  Created by Aleksei Kapranov on 10.04.2024.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface CoinRotationScreenSaverView : ScreenSaverView
+
+@end
